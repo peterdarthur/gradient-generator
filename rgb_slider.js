ccf822fd7797a7fccac4ffaf4aeeb3a865d70a11
Peter Arthur <peterarthur@Peters-MacBook-Pro.local>
2017-10-14 15:21:49 -0700

    now user can switch between existing sets with color, opacity, range intact and working

--- rgb_slider.js
+++ rgb_slider.js
@@ -4,8 +4,12 @@
 // ____ Create saved set code for import/export
 // ____ Create printable version
 
-var setCount = 1;
-var selectedSet = 1;
+var setCount = 1; // total number of sets
+var selectedSet = 1; // selected set #
+var r,g,b,a;
+
+
+// all set data (sets[0].r = first set's Red value)
 var sets = [
       {
           "r": 2,
@@ -15,8 +19,9 @@ var sets = [
           "leftPx": 20,
           "rightPx": 140
       }
-  ];
+];
 
+// convert #ffffff to rgba(255,255,255,1)
 function hexFromRGB(r, g, b) {
     var hex = [
       r.toString( 16 ),
@@ -33,112 +38,137 @@ function hexFromRGB(r, g, b) {
     return hex.join( "" ).toUpperCase();
 }
 
+// get random number between min and max
 function randomIntFromInterval(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function setRGBA(r,g,b,a){
-    var rgba = "rgba("+r+", "+g+", "+b+", "+a+")";
+// get data to pass into applyData() to set up a new set - must happen at page load and after random set data is generated
+// state is either 'init' or 0
+function getData(state) {
+  console.log("getData()");
+
+    r = sets[selectedSet-1].r,
+    g = sets[selectedSet-1].g,
+    b = sets[selectedSet-1].b,
+    a = sets[selectedSet-1].a,
+    leftPx = sets[selectedSet-1].leftPx,
+    rightPx = sets[selectedSet-1].rightPx;
+    // console.log('a: '+a);
+    // console.log('leftPx: ' + leftPx);
+    // console.log('rightPx: ' + rightPx);
+
+    applyData(r,g,b,a,leftPx,rightPx,state);
+}
+
+// runs within getData();
+// applies initial colors, ranges, opacity for the selected set
+function applyData(r,g,b,a,leftPx,rightPx,state){
+  console.log('applyData()');
+  var rgba = "rgba("+r+", "+g+", "+b+", "+a/100+")";
+
+  // once per set (at selectedSet init)
+  if (state === 'initSet') {
+    // add color and opacity to plaid set
     $( "#sw"+ selectedSet +" .swatch" ).css("background-color", rgba );
-    // console.log("sw color is "+rgba);
-    // console.log("opacity is"+a)
-    $( "#ui"+ selectedSet +" .color" ).val("Color: " + rgba );
-    $( "#ui"+ selectedSet +" .color" ).css("background-color", rgba );
-
-    // determine if text should be white or black over this color
+  }
+    
+  // every time selectedSet is changed
+    // add color and opacity to UI color sample
+    $( ".color" ).val("Color: " + rgba );
+    $( ".color" ).css("background-color", rgba );
+    // determine if text should be white or black over this color in UI color sample
     textColorUpdate(r,g,b,a);
+
+    // 
 }
 
+// set text on color sample to either white or black for best contrast
 function textColorUpdate(r,g,b,a) {
+  
   var whiteText = 0;
-    sumColors = r + g + b;
-    if (a < 0.4 || sumColors > 379) {
-      $( "#ui"+ selectedSet +" .color" ).css( "color", "#ffffff" );
-    } else {
-      $( "#ui"+ selectedSet +" .color" ).css( "color", "#000000" );
-    }
+  sumColors = r + g + b;
+  if (a > 40 || sumColors < 379) {
+    $( ".color" ).css( "color", "#ffffff" );
+  } else {
+    $( ".color" ).css( "color", "#000000" );
+  }
 }
 
-function initializeSwatch() {
-  // console.log("refreshSwatch()");
+function refreshSwatch(change,state) {
+  console.log("refreshSwatch()");
 
+    // get defaults to pass along if unchanged
     var r = sets[selectedSet-1].r,
         g = sets[selectedSet-1].g,
         b = sets[selectedSet-1].b,
-        a = sets[selectedSet-1].a/100;
+        a = (sets[selectedSet-1].a);
+        console.log("Slider Value: " + $( ".opacity" ).slider( "value" ));
 
-    setRGBA(r,g,b,a);
-}
+    // future use (dark vs light)
+    if (change === "all") { // console.log("all refresh");
 
-function refreshSwatch(s) {
-  // console.log("refreshSwatch()");
-
-    var r = sets[selectedSet-1].r,
-        g = sets[selectedSet-1].g,
-        b = sets[selectedSet-1].b,
-        a = sets[selectedSet-1].a/100;
-
-    if (s == "all") {
-      // console.log("all refresh");
-
-      //set value based on slider
       r = $( ".red" ).slider( "value" ),
       g = $( ".green" ).slider( "value" ),
-      b = $( ".blue" ).slider( "value" );
-      //update sets array
+      b = $( ".blue" ).slider( "value" ); //set values based on slider
       sets[selectedSet-1].r = r;
       sets[selectedSet-1].g = g;
-      sets[selectedSet-1].b = b;
+      sets[selectedSet-1].b = b; //update sets array
 
-    } else if (s == "red"){
-      // console.log("red refresh");
+    } else if (change === "red"){ // console.log("red refresh");
+      r = $( ".red" ).slider( "value" ); //set value based on slider
+      sets[selectedSet-1].r = r; //update sets array
 
-      //set value based on slider
-      r = $( ".red" ).slider( "value" );
-      //update sets array
-      sets[selectedSet-1].r = r;
+    } else if (change === "green") { // console.log("green refresh");
+      g = $( ".green" ).slider( "value" ); //set value based on slider
+      sets[selectedSet-1].g = g; //update sets array
 
-    } else if (s == "green") {
-            // console.log("green refresh");
+    } else if (change === "blue") { // console.log("blue refresh");
+      b = $( ".blue" ).slider( "value" ); //set value based on slider
+      sets[selectedSet-1].b = b; //update sets array
 
-      //set value based on slider
-      g = $( ".green" ).slider( "value" );
-      //update sets array
-      sets[selectedSet-1].g = g;
-
-    } else if (s == "blue") {
-            // console.log("blue refresh");
-
-      //set value based on slider
-      b = $( ".blue" ).slider( "value" );
-      //update sets array
-      sets[selectedSet-1].b = b;
+    } else if (change === "opacity") { // console.log("blue refresh");
+      a = $( ".opacity" ).slider( "value" ); //set value based on slider
+      sets[selectedSet-1].a = a; //update sets array
+      console.log('Opacity Updated in the Set to: '+sets[selectedSet-1].a);
+      console.log('Opacity decimal: '+a);
 
     }
 
-    setRGBA(r,g,b,a);
+    applyData(r,g,b,a,leftPx,rightPx,state);
 }
 
-function refreshWidthSlider(event,ui) {
-
-        var spaceOutside = ui.values[0];
-        var width = ui.values[1] - ui.values[0];
-        var spaceInside = 150 - ui.values[1];
-        $( ".stats" ).html("Outside: " + spaceOutside + "px<br>Width: " + width + "px<br>Inside: " + spaceInside + "px");
-        $( "#sw"+selectedSet+" .vert.a").css({"margin-left":spaceOutside, "width":width, "margin-right":spaceInside});
-        $( "#sw"+selectedSet+" .vert.b").css({"margin-left":spaceInside, "width":width, "margin-right":spaceOutside});
-        $( "#sw"+selectedSet+" .horiz.a").css({"margin-top":spaceOutside, "height":width, "margin-bottom":spaceInside});
-        $( "#sw"+selectedSet+" .horiz.b").css({"margin-top":spaceInside, "height":width, "margin-bottom":spaceOutside});
-        // console.log("width changed");
+function widthFromSlider(event,ui) {
+  console.log('widthFromSlider()');
+  updateWidth(ui.values[0],ui.values[1]);
 }
 
-function initializeSliders(state) {
-  // console.log("initializeSliders()");
+function updateWidth(leftPx,rightPx){
+  console.log('updateWidth()');
+  // update data
+  sets[selectedSet-1].leftPx = leftPx;
+  sets[selectedSet-1].rightPx = rightPx;
+  var spaceOutside = leftPx;
+  var width = rightPx - leftPx;
+  var spaceInside = 150 - rightPx;
+  $( ".stats" ).html("Outside: " + spaceOutside + "px<br>Width: " + width + "px<br>Inside: " + spaceInside + "px");
+  $( "#sw"+selectedSet+" .vert.a").css({"margin-left":spaceOutside, "width":width, "margin-right":spaceInside});
+  $( "#sw"+selectedSet+" .vert.b").css({"margin-left":spaceInside, "width":width, "margin-right":spaceOutside});
+  $( "#sw"+selectedSet+" .horiz.a").css({"margin-top":spaceOutside, "height":width, "margin-bottom":spaceInside});
+  $( "#sw"+selectedSet+" .horiz.b").css({"margin-top":spaceInside, "height":width, "margin-bottom":spaceOutside});
+}
+
+function initializeSliders() {
+  console.log("initializeSliders()");
+  
+  // get data for this set
   var r = sets[selectedSet-1].r,
       g = sets[selectedSet-1].g,
-      b = sets[selectedSet-1].b;
-
-  if (state !== "initial") { $( ".red, .green, .blue", ".opacity" ).slider("destroy");}
+      b = sets[selectedSet-1].b,
+      a = sets[selectedSet-1].a,
+      leftPx = sets[selectedSet-1].leftPx,
+      rightPx = sets[selectedSet-1].rightPx,
+      state = 'initSet';
 
     // console.log("initializing red slider");
     $( ".red" ).slider({
@@ -146,8 +176,8 @@ function initializeSliders(state) {
       range: "min",
       max: 255,
       value: r,
-      slide: function(){refreshSwatch("red")},
-      change: function(){refreshSwatch("red")}
+      slide: function(){refreshSwatch("red",state)},
+      change: function(){refreshSwatch("red",state)}
     });
     // console.log("red initialized");
     $( ".green" ).slider({
@@ -155,8 +185,8 @@ function initializeSliders(state) {
       range: "min",
       max: 255,
       value: g,
-      slide: function(){refreshSwatch("green")},
-      change: function(){refreshSwatch("green")}
+      slide: function(){refreshSwatch("green",state)},
+      change: function(){refreshSwatch("green",state)}
     });
     // console.log("green initialized");
     $( ".blue" ).slider({
@@ -164,8 +194,8 @@ function initializeSliders(state) {
       range: "min",
       max: 255,
       value: b,
-      slide: function(){refreshSwatch("blue")},
-      change: function(){refreshSwatch("blue")}
+      slide: function(){refreshSwatch("blue",state)},
+      change: function(){refreshSwatch("blue",state)}
     });
     // console.log("blue initialized");
 
@@ -174,48 +204,74 @@ function initializeSliders(state) {
       range: true,
       min: 0,
       max: 150,
-      values: [ sets[selectedSet-1].leftPx , sets[selectedSet-1].rightPx ],
+      values: [ leftPx , rightPx ],
       slide: function(event,ui) {
-        refreshWidthSlider(event,ui)
+        widthFromSlider(event,ui)
       }
     });
 
     // Set Swatch Divs to reflect initialized LeftPx and RightPx values
     var leftPx = sets[selectedSet-1].leftPx;
     var rightPx = sets[selectedSet-1].rightPx;
-        var spaceOutside = leftPx;
-        var width = rightPx - leftPx;
-        var spaceInside = 150 - rightPx;
-        $( ".stats" ).html("Outside: " + spaceOutside + "px<br>Width: " + width + "px<br>Inside: " + spaceInside + "px");
-        $( "#sw"+selectedSet+" .vert.a").css({"margin-left":spaceOutside, "width":width, "margin-right":spaceInside});
-        $( "#sw"+selectedSet+" .vert.b").css({"margin-left":spaceInside, "width":width, "margin-right":spaceOutside});
-        $( "#sw"+selectedSet+" .horiz.a").css({"margin-top":spaceOutside, "height":width, "margin-bottom":spaceInside});
-        $( "#sw"+selectedSet+" .horiz.b").css({"margin-top":spaceInside, "height":width, "margin-bottom":spaceOutside});
-
-
+    updateWidth(leftPx,rightPx);
+        
     // opacity
     $( ".opacity" ).slider({
       min: 0,
       max: 100,
-      value: (sets[selectedSet-1].a)/100,
-      slide: function( event, ui ) {
-        var a = ui.value/100,
-        r = sets[selectedSet-1].r,
-        g = sets[selectedSet-1].g,
-        b = sets[selectedSet-1].b;
+      value: a,
+      slide: function(){refreshSwatch("opacity",state)},
+      change: function(){refreshSwatch("opacity",state)}
+      // slide: function( event, ui ) {
+      //   var a = ui.value/100,
+      //   r = sets[selectedSet-1].r,
+      //   g = sets[selectedSet-1].g,
+      //   b = sets[selectedSet-1].b;
         
-        setRGBA(r,g,b,a);
-      }
+      //   applyData(r,g,b,a);
+      // }
     });
 }
 
-$(function(){
+//
+function updateSliders() {
+  console.log("updateSliders()");
+  var r = sets[selectedSet-1].r,
+      g = sets[selectedSet-1].g,
+      b = sets[selectedSet-1].b,
+      a = sets[selectedSet-1].a,
+      leftPx = sets[selectedSet-1].leftPx,
+      rightPx = sets[selectedSet-1].rightPx;
 
-      initializeSliders("initial");
-      initializeSwatch();
+  // if (state !== "initial") { $( ".red, .green, .blue", ".opacity" ).slider("destroy");}
+
+    // console.log("updating red slider");
+    $( ".red" ).slider({value: r});
+    // console.log("red updated");
+    $( ".green" ).slider({value: g});
+    // console.log("green updated");
+    $( ".blue" ).slider({value: b});
+    // console.log("blue updated");
+
+    // update position and width slider
+    $( ".range" ).slider({values: [ leftPx , rightPx ]});
+
+    // Set Swatch Divs to reflect initialized LeftPx and RightPx values
+    updateWidth(leftPx,rightPx);
+
+    // opacity
+    $( ".opacity" ).slider({value: a});
+}
+
+$(function(){
+  
+      // only on page load
+      initializeSliders();
+      getData('initSet');
       
       // add new
-      $( ".add-new" ).click(function(){     //console.log("click plus");
+      $( ".add-new" ).click(function(){     
+        console.log("click plus");
         setCount++; 
         selectedSet = setCount;
         // console.log('setCount and selectedSet is ' + setCount);
@@ -223,36 +279,50 @@ $(function(){
         $('#sw1').clone().attr('id', 'sw'+ setCount).insertAfter($('#sw'+(setCount-1))); // add new swatches on top of previous ones
         $('button.select-set[data-set="1"]').clone().attr('data-set', setCount).html('Set ' + setCount).insertAfter($('button.select-set[data-set="'+(setCount-1)+'"]')); // add new Set Select Button
         
-        var randomRangeA = randomIntFromInterval(0,253);
-        var randomRangeB = randomIntFromInterval(randomRangeA,255);
+        var randomRangeA = randomIntFromInterval(0,148);
+        var randomRangeB = randomIntFromInterval(randomRangeA,150);
 
-        var randomSetValues = [];
-        randomSetValues.push(randomIntFromInterval(0,255));
-        randomSetValues.push(randomIntFromInterval(0,255));
-        randomSetValues.push(randomIntFromInterval(0,255));
-        randomSetValues.push(randomIntFromInterval(10,60));
-        randomSetValues.push(randomRangeA);
-        randomSetValues.push(randomRangeB);
+      //   var randomSetValues = [];
+      //   randomSetValues.push(randomIntFromInterval(0,255)); // r
+      //   randomSetValues.push(randomIntFromInterval(0,255)); // g
+      //   randomSetValues.push(randomIntFromInterval(0,255)); // b
+      //   randomSetValues.push(randomIntFromInterval(10,60)); // a
+      //   randomSetValues.push(randomRangeA);                 // leftPx
+      //   randomSetValues.push(randomRangeB);                 // rightPx
 
-        console.log(randomSetValues);
+      //   console.log(randomSetValues);
 
-       var newSet = {
-          "r": randomSetValues[0],
-          "g": randomSetValues[1],
-          "b": randomSetValues[2],
-          "a": randomSetValues[3],
-          "leftPx": randomSetValues[4],
-          "rightPx": randomSetValues[5]};
-       sets.push(newSet);
-       // console.log(sets);
+      //  var newSet = {
+      //     "r": randomSetValues[0],
+      //     "g": randomSetValues[1],
+      //     "b": randomSetValues[2],
+      //     "a": randomSetValues[3],
+      //     "leftPx": randomSetValues[4],
+      //     "rightPx": randomSetValues[5]};
+      //  sets.push(newSet);
+      //  // console.log(sets);
+
+      var newSet = {
+         "r": randomIntFromInterval(0,255),
+         "g": randomIntFromInterval(0,255),
+         "b": randomIntFromInterval(0,255),
+         "a": randomIntFromInterval(10,60),
+         "leftPx": randomRangeA,
+         "rightPx": randomRangeB
+        };
+      sets.push(newSet);
+      console.log(sets);
 
        initializeSliders();
-       initializeSwatch();
+       getData('initSet');
       }); 
 
       $("body").on("click", ".select-set", function(){
         selectedSet = $(this).attr("data-set");
         console.log('selectedSet is ' + selectedSet);
+        getData(0); // not initial, no need to update swatch bars, just picker UI
+        initializeSliders();
+        
       });
 
 
